9c2866757b716f5627a53903aedd61e83d43645e
Kerim Imamovic <79793398+kimamovic21@users.noreply.github.com>
2022-05-12 16:17:49 +0200

    Add files via upload

---
+++ gallery2.js
@@ -0,0 +1,8 @@
+new mySwipper(".swiper-container",  {
+    speed: 400,
+    spaceBetween: 100,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+});
